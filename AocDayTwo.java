7898a21197bc1f90b4eb41abf3202232e2ad4904
mary_m <mathews.mary@gmail.com>
2020-12-07 17:32:44 +0530

    Day 2 Part 2 solution

--- AocDayTwo.java
+++ AocDayTwo.java
@@ -3,18 +3,19 @@ import java.util.*;
 
 class AocDayTwo {
     final String regexPattern = "[0-9]+-[0-9]+\s[a-z]:\s[a-z]+";
+    /* Set 2nd input to isValidPassword() to false for part 1 and true for part 2 */
     public static void main(String[] args) throws IOException {
         AocDayTwo obj = new AocDayTwo();
         int validPasswordCount = 0;
         ArrayList<String> puzzleInput = obj.inputPuzzle();
         for(String puzzle : puzzleInput) {
-            if(obj.validateInput(puzzle) && obj.isValidPassword(puzzle))
+            if(obj.validateInput(puzzle) && obj.isValidPassword(puzzle, false))
                 validPasswordCount++;
         }
         System.out.println("\n" + validPasswordCount);
     }
 
-    /* Enter CTRL + D (Mac OS) or CTRL + Z (Windows) to terminate input (EOF character) */
+    /* Enter Enter and CTRL + D (Mac OS) or Enter and CTRL + Z (Windows) to terminate input (EOF character) */
     ArrayList<String> inputPuzzle() {
         ArrayList<String> input = new ArrayList<String>();
         Scanner in = new Scanner(System.in);
@@ -29,18 +30,22 @@ class AocDayTwo {
         return value.matches(regexPattern);
     }
 
-    boolean isValidPassword(String value) {
-        int minCount = 0, maxCount = 0;
+    boolean isValidPassword(String value, boolean isNew) {
+        int firstNumber = 0, secondNumber = 0;
         char letterToFind = ' ';
         String password = "";
         try {
             String[] hypenSplitString = value.split("-");
             String[] spaceSplitString = hypenSplitString[1].split(" ");
-            minCount = Integer.parseInt(hypenSplitString[0]);
-            maxCount = Integer.parseInt(spaceSplitString[0]);
+            firstNumber = Integer.parseInt(hypenSplitString[0]);
+            secondNumber = Integer.parseInt(spaceSplitString[0]);
             letterToFind = spaceSplitString[1].charAt(0);
             password = spaceSplitString[2];
-            return isValid(minCount, maxCount, letterToFind, password);
+            if(isNew) {
+                return isValidAccordingToNewRules(firstNumber, secondNumber, letterToFind, password);
+            } else {
+                return isValidAccordingToOldRules(firstNumber, secondNumber, letterToFind, password);
+            }
         }
         catch(Exception e) {
             e.printStackTrace();
@@ -48,12 +53,29 @@ class AocDayTwo {
         return false;
     }
 
-    boolean isValid(int min, int max, char letter, String password) {
+    boolean isValidAccordingToOldRules(int min, int max, char letter, String password) {
         int count = 0;
         for(int i = 0; i < password.length(); i++) {
             if(password.charAt(i) == letter)
                 count++;
         }
         return (min <= count && count <= max);
+    }
+
+    boolean isValidAccordingToNewRules(int pos1, int pos2, char letter, String password) {
+        boolean characterAtPosition1 = false, characterAtPosition2 = false;
+        pos1--;
+        pos2--;
+        if(isValidIndex(pos1, password.length() - 1) && password.charAt(pos1) == letter)
+            characterAtPosition1 = true;
+        if(isValidIndex(pos2, password.length() - 1) && password.charAt(pos2) == letter)
+            characterAtPosition2 = true;
+        if(characterAtPosition1 && characterAtPosition2)
+            return false;
+        return (characterAtPosition1 || characterAtPosition2);       
+    }
+
+    boolean isValidIndex(int index, int maxIndex) {
+        return (index >=0 && index <= maxIndex);
     }
 }
