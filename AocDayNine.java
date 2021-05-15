90f04d9952b990d3f9be8059ef861b4da13cc162
mary_m <mathews.mary@gmail.com>
2021-05-14 19:23:12 -0500

    Day 9 Part 2 solution

--- AocDayNine.java
+++ AocDayNine.java
@@ -4,10 +4,14 @@ import java.lang.*;
 class AocDayNine {
     public static void main(String[] args) {
         AocDayNine obj = new AocDayNine();
-
         ArrayList<Long> input = new ArrayList<Long>();
+
         obj.getInput(input);
-        System.out.println(obj.findWeakNumber(input));
+        Long weakNumber = obj.findWeakNumber(input);
+        Long weakness = obj.getEncryptionWeakness(weakNumber, input);
+
+        System.out.println(weakNumber);
+        System.out.println(weakness);
     }
 
     void getInput(ArrayList<Long> input) {
@@ -29,7 +33,7 @@ class AocDayNine {
         HashSet<Long> set = new HashSet<Long>();
         int removeIndex = 0;
         int returnIndex = 0;
-        for(int i = 0; i < 24; i++)
+        for(int i = 0; i <= 24; i++)
             set.add(input.get(i));
         for(int i = 25; i < input.size(); i++) {
             if(!checkTwoSum(input.get(i), set)) {
@@ -52,5 +56,40 @@ class AocDayNine {
                 return true;
         }
         return false;
+    }
+
+    Long getEncryptionWeakness(Long target, ArrayList<Long> input) {
+        Long sum = input.get(0);
+        int start = 0;
+        int end = 0;
+
+        for(end = 1; end < input.size();) {
+            if(input.get(end) > target) {
+                end++;
+                start = end;
+                continue;
+            }
+            Long newSum = sum + input.get(end);
+            if(newSum.equals(target)) {
+                break;
+            }
+            else if(newSum < target) {
+                sum = newSum;
+                end++;
+            }
+            else {
+                sum = sum - input.get(start);
+                start++;
+            }
+        }
+        
+        Long min = input.get(start);
+        Long max = input.get(start);
+        for(int i = start; i <= end; i++) {
+            min = input.get(i) < min ? input.get(i) : min;
+            max = input.get(i) > max ? input.get(i) : max;
+        }
+
+        return (min + max);
     }
 }
