03107e17f21036fbfbaa4884072f4567606369cc
mary_m <mathews.mary@gmail.com>
2022-01-03 18:07:02 -0600

    cleaned up code

--- AocDayEleven.py
+++ AocDayEleven.py
@@ -10,66 +10,68 @@ offsets = [
     [-1, -1]
 ]
 
-with open('input_eleven.txt') as file:
-    data = file.readlines()
-    for line in data:
-        matrix.append(line.rstrip("\n"))
+def read_input():
+    with open('input_eleven.txt') as file:
+        data = file.readlines()
+        for line in data:
+            matrix.append(line.rstrip("\n"))
 
-old_matrix = matrix.copy()
-new_matrix = matrix.copy()
+def count_occupied_seats(matrix):
+    occupied_seats = 0
 
-while True:
+    for r in matrix:
+        for c in r:
+            if c == '#':
+                occupied_seats += 1
     
-    old_matrix = new_matrix.copy()
+    return occupied_seats
 
-    for r, row in enumerate(old_matrix):
-        for c, col in enumerate(row):
+def get_adjacent_occupied_count(r, c, row, old_matrix):
+    count = 0
 
-            if col == 'L':
-                occupied = False 
+    for offset in offsets:
+        pos_r = r + offset[0]
+        pos_c = c + offset[1]
 
-                for offset in offsets:
-                    pos_r = r + offset[0]
-                    pos_c = c + offset[1]
+        if pos_r >= len(matrix) or pos_r < 0 or pos_c >= len(row) or pos_c < 0:
+            continue
 
-                    if pos_r >= len(matrix) or pos_r < 0 or pos_c >= len(row) or pos_c < 0:
-                        continue
+        if old_matrix[pos_r][pos_c] == '#':
+            count += 1
+        
+    return count
 
-                    if old_matrix[pos_r][pos_c] == '#':
-                        occupied = True
-                        break
-                    
-                if not occupied:
-                    update = list(new_matrix[r])
-                    update[c] = '#'
-                    new_matrix[r] = ''.join(update)
-            
-            elif col == '#':
-                occ_count = 0
+def update_matrix():
+    old_matrix = matrix.copy()
+    new_matrix = matrix.copy()  
 
-                for offset in offsets:
-                    pos_r = r + offset[0]
-                    pos_c = c + offset[1]
+    while True:
+        old_matrix = new_matrix.copy()
 
-                    if pos_r >= len(matrix) or pos_r < 0 or pos_c >= len(row) or pos_c < 0:
-                        continue
-
-                    if old_matrix[pos_r][pos_c] == '#':
-                        occ_count += 1
+        for r, row in enumerate(old_matrix):
+            for c, col in enumerate(row):
                 
-                if occ_count >= 4:
+                update_char = ''
+                adj_occ_count = get_adjacent_occupied_count(r, c, row, old_matrix)
+
+                if col == 'L':
+                    if adj_occ_count == 0:
+                        update_char = '#'
+                
+                elif col == '#':
+                    if adj_occ_count >= 4:
+                        update_char = 'L'
+
+                if update_char != '':
                     update = list(new_matrix[r])
-                    update[c] = 'L'
+                    update[c] = update_char
                     new_matrix[r] = ''.join(update)
-    
-    if new_matrix == old_matrix:
-        break
+        
+        if new_matrix == old_matrix:
+            break
 
-occupied_seats = 0
+    return new_matrix
 
-for r in new_matrix:
-    for c in r:
-        if c == '#':
-            occupied_seats += 1
-
-print(occupied_seats)
+read_input()
+new_matrix = update_matrix()
+print(count_occupied_seats(new_matrix))
